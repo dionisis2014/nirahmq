6e3ba5a0a7d80a82d387a12f68269686d97de18b
Dionisis Toulatos <toulatosdionisis@gmail.com>
2025-05-26 02:30:50 +0300

    docs(utils): Add docstrings
    Signed-off-by: Dionisis Toulatos <toulatosdionisis@gmail.com>

--- src/nirahmq/utils.py
+++ src/nirahmq/utils.py
@@ -15,6 +15,8 @@ class _Unset(Enum):
 
 
 Unset = _Unset.TOKEN
+"""A custom singleton type used by to indicate a value is unset."""
+
 type Optional[T] = T | _Unset
 
 type Required = None  # Required fields should always be defined like `Annotated[<actual type>, Required]`
@@ -29,9 +31,18 @@ def _validate_regex_string(value: str) -> str:
 
 
 type Regex = Annotated[str, AfterValidator(_validate_regex_string)]
+"""A custom type alias used by :py:mod:`nirahmq` to validate a string is a valid regex pattern."""
 
 
 def sanitize_string(string: str) -> str:
+    """Sanitizes a string for use as an MQTT topic.
+
+    .. note:: The sanitization is not an MQTT restriction but a Home Assistant one.
+
+    :param str string: The string to be sanitized
+    :return: The sanitized string
+    :rtype: str
+    """
     # Replace whitespace character between non-whitespace ones with single underscore
     string = re.sub(r"(\S)\s+(\S)", r"\1_\2", string)
     # Replace non-valid characters with nothing (remove them)
@@ -41,10 +52,20 @@ def sanitize_string(string: str) -> str:
 
 
 def clamp[T](val: T, min_: T, max_: T) -> T:
+    """Clamp a value in the range :math:`[min\\_, max\\_]`.
+
+    :param T val: The value to be clamped
+    :param T min\\_: The minimum allowed value (inclusive)
+    :param T max\\_: The maximum allowed value (inclusive)
+    :return: The clamped value
+    :rtype: T
+    """
     return min(max(val, min_), max_)
 
 
 class BaseModel(PydanticBaseModel):
+    """A customized :py:class:`pydantic.BaseModel` class for use by the rest of the library."""
+
     model_config = ConfigDict(
         validate_by_alias=True,
         validate_by_name=True,
